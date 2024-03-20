a6bd512a9e3443790e527a80a202b53c5356cdc3
hiro501 <hiro0501saito@gmail.com>
2024-03-21 00:32:35 +0900

    編集の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :destroy,]
+  before_action :authenticate_user!, except: [:index, :show, :destroy]
+  before_action :set_prototype, only: [:edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
     @prototypes = Prototype.all
@@ -25,11 +26,9 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -38,7 +37,6 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     @prototype.destroy
     redirect_to root_path
   end
@@ -46,6 +44,10 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
   def move_to_index
